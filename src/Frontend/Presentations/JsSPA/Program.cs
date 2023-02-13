e6c936e805ded582a84742e5dbf4114cf79dd6d8
stefan27bg@abv.bg <stefan27bg@abv.bg>
2023-02-13 19:46:43 +0100

    Converted to the new SPA APP - more lightweight

--- src/Frontend/Presentations/JsSPA/Program.cs
+++ src/Frontend/Presentations/JsSPA/Program.cs
@@ -5,8 +5,14 @@ var app = builder.Build();
 
 
 
-// Dev - Mode
-if (app.Environment.IsDevelopment())
+// If Production - Mode 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts(); // Https
+    app.UseHttpsRedirection(); // Redirect from http to https
+}
+else
 {
     app.UseDeveloperExceptionPage();
 }
@@ -19,16 +25,12 @@ if (app.Environment.IsDevelopment())
 app.UseDefaultFiles(); // Means search for the index.html or default files in wwwroot - 
 app.UseStaticFiles(); // Static files are files in the wwwroot - Use wwwroot files
 
-
-// Https
-app.UseHsts();
-app.UseHttpsRedirection();
+ 
 app.UseRouting();
 
 
 // Endpoints
-app.MapFallbackToFile("/index.html");  // Route to the index.html if 404 Error and for SPA it always routes to index because SPA is single page APP.  //This is not url routing for the SPA Views - The url routing is done on the cLient side with javascript. 
-
+app.MapFallbackToFile("index.html");  // Route to the index.html if 404 Error and for SPA it always routes to index because SPA is single page APP.  //This is not url routing for the SPA Views - The url routing is done on the cLient side with javascript. 
 
 app.Run();
 
--- src/Frontend/Presentations/JsSPA/wwwroot/static/js/main.js
+++ src/Frontend/Presentations/JsSPA/wwwroot/static/js/main.js
@@ -1,124 +1,53 @@
-﻿// Imports
-import Home from "./views/Home.js";
-import Login from "./views/Login.js";
-import Register from "./views/Register.js";
-//import Posts from "./views/Posts.js";
-//import Profile from "./views/Profile.js";
-//import Profile from "./views/Profile.js";
-//import MongoGraph from "./views/MongoGraph.js";
-//import { GetUserEmailAsString, UpdateUserHtml } from '/static/js/resources/default_scripts.js';
-
-// Previous VIEW / PAAGE -------------------------------------------------------------------->
-export let prevView = ['/', '/'];
+﻿// Imports ------------------------------------------------------------------------------------------------
+import * as Home from "./views/Home.js";
+import * as Login from "./views/Login.js";
+import * as Register from "./views/Register.js";
 
 
-
-// Navigator--------------------------------------------------------------------------------->
-export const navigateTo = url => {
-    history.pushState(null, null, url); // Add the url to the history API of Js   
-    router();
-}; 
-
-
-
-
-// Router------------------------------------------------------------------------------------------>
-const router = async () => {
-
-    // Declaration of the ROUTES
-    // Map Views to paths - so on specific path there will be specific veiew that will show specific HTML ---------------------------------------------------------->
-    const routes = [
-        { path: "/", view: Home }, // On Path "/" use the HomeView class and inject html in the #app div
-        { path: "/Login", view: Login },
-        { path: "/Register", view: Register }
-    
-    ];
+// Router  ------------------------------------------------------------------------------------------------
+const router = (e) => {
+    e = event || window.event;
+    e.preventDefault(); // Prevent deafult behavior don't follow the link
+    window.history.pushState(null, null, e.target.href); // Add the url to the history api of js so we can navigate back and forth with the browser buttons
+    handleLocation();
+};
 
 
-
-
-
-    // Test each route for potential match ------------------------------------------------------->
-    // Get the current Url and check if its defined in routes method "Check if its one of our Spa Urls" ----------------------------------------------------->
-    const potentialMatches = routes.map(route => {   // The Map method loops routes and stores each route as variable route
-        return {
-            route: route,
-            isMatch: location.pathname === route.path  // True if match else false - location.pathname = the path of the current page / View
-        };
-    });
-
-
-
-
-    // Check if there is Match------------------------------------------------------------------->
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);  // Get isMatch from potentialMatches
-
-
-
-    // If no match return to StartPage "Home"
-    if (!match) {
-        match = {
-            route: routes[0], // StartPage
-            isMatch: true
-        };
-    }
-
-
-
-
-    // PREVIOUS VIEW / PAGE --------------------------------------------------------------------->
-    //if (location.pathname != '/Login' && location.pathname != '/Register')  // If Page Login or Register etc. dont add them to the prev view
-    //{
-    //    prevView.push(location.href); // Add View to PreviousView so when needed we can navigate back to previous Page/View 
-    //    prevView.shift(); // Remove the First element from the array
-    //}
-
-
-    // Check if User logged in than show Content - ex. Prifile - Update etc. Ex. for specific Views password may be required, so here we do the check for those Views that reqire password.
-    //if (location.pathname == '/Profile' || location.pathname == '/' || location.pathname == '/MongoGraph' || location.pathname == '/Search') {
-    //    if (await GetUserEmailAsString() == '') {
-    //        match = {
-    //            route: routes[4], // Route to LogIn if not logged in
-    //            isMatch: true
-    //        };
-    //    }
-    //}
-
-    const view = new match.route.view(); // If match  use the routes array of the router and get the view function for the route
-    document.getElementById("app").innerHTML = await view.getHtml();  // Get the #app div and use the view function to inject Html in it from the view class ex."Dashboard, Posts, Settings etc."
-    await view.executeViewScript(); // Execute the script for the specific View
+//Routes ------------ The defined routes of the SPA APP ---------------------------------------------------
+const routes = {
+    '/': Home, // On Path "/" use the HomeView class and inject html in the #app div
+    '/Login': Login,
+    '/Register': Register
 };
 
 
 
+// The method that gets the current view and injects it in the "app"" container div.
+// Handle location  ---------------------------------------------------------------------------------------
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const currentRoute = routes[path] || routes['/']; // If there is no match go to Home "/" if the url is not found in the "routes object" than load Home View
+    document.getElementById("app").innerHTML = await currentRoute.getHtmlAsync(); // Get the Html code for the specific View
+    await currentRoute.executeViewScriptAsync(); // Execute the View script "If there is specific script to be executet to the specific view"
+};
 
 
 // On-Navigating-Back&Forth-Load the Content--Together with the url------------------------------------------------------------------------------------>
-window.addEventListener("popstate", router); // On popstate "If back button is pressed" use the router array to load back the previeous SPA View
+window.addEventListener("popstate", handleLocation); // On popstate "If back button is pressed" use the method to load back the previeous SPA View
+
+
 
 
 // Listen for document fully Loaded
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // On Dom loaded add bodyEventlistener to listen for click in the body
     document.body.addEventListener("click", e => { //Listen for click in the body
-        if (e.target.matches("[data-link]")) {  // If body item was clicked and it is data-link decorated
-            e.preventDefault();  // Prevent deafult behavior dont follow the link
-            navigateTo(e.target.href);  // Navigate method   
+        if (e.target.matches("[data-link]")) {  // If body item was clicked and it is data-link decorated 
+            router(); // Load the content if the url is defined in our "Spa Urls"
         }
     });
-
-
-    router(); // Load the content if the url is defined in our "Spa Urls"
 });
 
-//#### Client Routing END #####
+
+handleLocation(); // Handle location on page load otherwise the location will be handled on decorated link with data-link click or on popstate a.k.a on back and forth browser button click with window.addEventListener("popstate"...
 
 
-
-
-
-
-
-
-
-// Load user Html ------------------------------------------------------------------------------------------------------------------------------------->
-//document.body.onload = function () { return UpdateUserHtml() }; // On Body Load - load UserHtml - username, logOut etc.
